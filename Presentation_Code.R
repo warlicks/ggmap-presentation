a3afcd1e8cb0eb2025354bfeb3cacd6558abd5eb
warlicks <warlick.sean@gmail.com>
2015-03-09 22:08:57 -0400

    Updated Code to include distance computation and route tracking

--- Presentation_Code.R
+++ Presentation_Code.R
@@ -38,20 +38,19 @@ map
 map<-ggmap(get_map(location = '1600 Pennslyvainia Ave NW, Washington, DC')) 
 
 ## Provide a Longitude and Latitude to Cetner The Map
-map<-ggmap(get_map(location=c(lon = -77.00455, lat = 38.90893), zoom = 12, maptype = "road", source = 'google')) #Using Latitude and Longitude
+map<-ggmap(get_map(location=c(lon = -77.00455, lat = 38.90893), zoom = 12, maptype = "roadmap", source = 'google')) #Using Latitude and Longitude
 map
 
 ## Provide Four Corners of The Map
 box<-make_bbox(lon = longitude, lat = latitude, data = weekday_bike)
-map<-ggmap(get_map(location = box, source = 'google', zoom = 12, maptype = 'road'))
-map<-ggplot(map, extent = "device", legend = "topright") #Convert to ggplot object
+map<-ggmap(get_map(location = box, source = 'google', zoom = 12, maptype = 'roadmap'))
 
 ## Add Detail to the map
 map + geom_point(data = weekday_bike, aes(x = longitude, y = latitude, size = average))
 
 map + geom_point(data = weekday_bike, aes(x = longitude, y = latitude, colour = average))
 
-map + geom_polygon(aes(x, y, linetype = type), data = df, fill = NA, colour = "black", inherit.aes = TRUE)
+map + geom_point(data = weekday_bike, aes(x = longitude, y = latitude)) + geom_polygon(aes(x, y, linetype = type), data = df, fill = NA, colour = "black", inherit.aes = TRUE)
 
 
 # Geo Coding
@@ -66,6 +65,23 @@ address
 ## Create an Intersection
 geocode(location = "G St NW & 22nd St NW, Washington, DC")
 map<-ggmap(get_map(location = c(lon = -98.01001, lat = 41.11103)))
+map  #Intersections Don't Seem to Work.
+
+# Geocode a State
+geocode(location = 'Iowa')
+map<-ggmap(get_map(location = c(lon = -93.0977, lat = 41.874), source = 'google', zoom = 7))
 map
 
-#Intersections Don't Seem to Work.  
+## Geocode Several Locations At Once
+cordinates<-geocode(location = c("Hartsfield - Jackson Atlanta International", "LAX", 'ORD', 'DFW', 'Denver International', 'JFK', 'SFO', 'CLT', 'LAS', 'PHX', 'Miami International', 'IAH', 'EWR', 'MCO', 'SEA', 'MSP', 'DTW', 'BOS', 'PHL', 'LGA'))
+
+##  Check to see how many calls too Google API we have left. 
+geocodeQueryCheck()
+
+#Compute Distances
+distance<-mapdist(weekday_bike[name == "Kennedy Center", as.numeric(list(longitude, latitude))], weekday_bike[name == "White House", as.numeric(list(longitude, latitude))], mode = 'bicycling')
+
+#Get Directions
+directions<-route(weekday_bike[name == "Kennedy Center", as.numeric(list(longitude, latitude))], weekday_bike[name == "White House", as.numeric(list(longitude, latitude))], mode = 'bicycling')
+
+map + geom_segment(data = directions, aes(x = startLon, y = startLat, xend = endLon, yend = endLat))
