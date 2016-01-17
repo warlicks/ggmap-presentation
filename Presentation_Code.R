0503ae7b4bf01a3739b286381cc1e28c5bd4c7bf
warlicks <warlick.sean@gmail.com>
2016-01-17 13:55:34 -0800

    Updated geocode calls to use google as source

--- Presentation_Code.R
+++ Presentation_Code.R
@@ -13,31 +13,31 @@ library(ggplot2)
 
 # Load Data
 ###############################################################################
-bike<-fread("presentation_data.csv", header = TRUE, sep = ',')
+bike <- fread("presentation_data.csv", header = TRUE, sep = ',')
 
 ##Cluster Cirlce Data
-angle<-seq(-pi,pi,length=50)
-df<-data.frame(x= 0.01351351 * sin(angle) - 77.038319, y = 0.01351351 * cos(angle) + 38.931150)
+angle <- seq(-pi,pi,length=50)
+df <- data.frame(x= 0.01351351 * sin(angle) - 77.038319, y = 0.01351351 * cos(angle) + 38.931150)
 df$type = rep("Departure", 50)
 
 # Getting The Map
 ###############################################################################
 
 ## Name a Landmark
-map<-get_map(location = "White House")
-map<-ggmap(map)
+map <- get_map(location = "White House")
+map <- ggmap(map)
 map
 
 ## Provide and Address
-map<-ggmap(get_map(location = '1600 Pennslyvainia Ave NW, Washington, DC')) 
+map <- ggmap(get_map(location = '1600 Pennslyvainia Ave NW, Washington, DC')) 
 
 ## Provide a Longitude and Latitude to Cetner The Map
-map<-ggmap(get_map(location=c(lon = -77.00455, lat = 38.90893), zoom = 12, maptype = "roadmap", source = 'google')) #Using Latitude and Longitude
+map <- ggmap(get_map(location=c(lon = -77.00455, lat = 38.90893), zoom = 12, maptype = "roadmap", source = 'google')) #Using Latitude and Longitude
 map
 
 ## Provide Four Corners of The Map
-box<-make_bbox(lon = longitude, lat = latitude, data = bike)
-map<-ggmap(get_map(location = box, source = 'google', zoom = 12, maptype = 'roadmap'))
+box <- make_bbox(lon = longitude, lat = latitude, data = bike)
+map <- ggmap(get_map(location = box, source = 'google', zoom = 12, maptype = 'roadmap'))
 
 ## Add Detail to the map
 map + geom_point(data = bike, aes(x = longitude, y = latitude, size = average))
@@ -50,35 +50,35 @@ map + geom_point(data = bike, aes(x = longitude, y = latitude)) + geom_polygon(a
 # Geo Coding
 ###############################################################################
 ## Name a Landmark
-geocode(location = "White House")
+geocode(location = "White House", source = "google")
 
 ## Set an Address
-address<-geocode(location = "2201 G St. NW, Washington, DC")
+address <- geocode(location = "2201 G St. NW, Washington, DC", source = "google")
 address
 
 ## Create an Intersection
-geocode(location = "G St NW & 22nd St NW, Washington, DC")
-map2<-ggmap(get_map(location = c(lon = -98.01001, lat = 41.11103)))
+geocode(location = "G St NW & 22nd St NW, Washington, DC", source = "google")
+map2 <- ggmap(get_map(location = c(lon = -98.01001, lat = 41.11103)))
 map2  #Intersections Don't Seem to Work.
 
 # Geocode a State
-geocode(location = 'Iowa')
-map3<-ggmap(get_map(location = c(lon = -93.0977, lat = 41.874), source = 'google', zoom = 7))
+geocode(location = 'Iowa', source = "google")
+map3 <- ggmap(get_map(location = c(lon = -93.0977, lat = 41.874), source = 'google', zoom = 7))
 map3
 
 ## Geocode Several Locations At Once
-cordinates<-geocode(location = c("Hartsfield - Jackson Atlanta International", "LAX", 'ORD', 'DFW', 'Denver International', 'JFK', 'SFO', 'CLT', 'LAS', 'PHX', 'Miami International', 'IAH', 'EWR', 'MCO', 'SEA', 'MSP', 'DTW', 'BOS', 'PHL', 'LGA'))
+cordinates <- geocode(location = c("Hartsfield - Jackson Atlanta International", "LAX", 'ORD', 'DFW', 'Denver International', 'JFK', 'SFO', 'CLT', 'LAS', 'PHX', 'Miami International', 'IAH', 'EWR', 'MCO', 'SEA', 'MSP', 'DTW', 'BOS', 'PHL', 'LGA'), source = "google")
 
 ##  Check to see how many calls too Google API we have left. 
 geocodeQueryCheck()
 
 #Compute Distances
 ###############################################################################
-distance<-mapdist(bike[name == "Kennedy Center", as.numeric(list(longitude, latitude))], bike[name == "White House", as.numeric(list(longitude, latitude))], mode = 'bicycling')
+distance <- mapdist(bike[name == "Kennedy Center", as.numeric(list(longitude, latitude))], bike[name == "White House", as.numeric(list(longitude, latitude))], mode = 'bicycling')
 
 #Get Directions
 ###############################################################################
-directions<-route(bike[name == "Kennedy Center", as.numeric(list(longitude, latitude))], bike[name == "White House", as.numeric(list(longitude, latitude))], mode = 'bicycling')
+directions <- route(bike[name == "Kennedy Center", as.numeric(list(longitude, latitude))], bike[name == "White House", as.numeric(list(longitude, latitude))], mode = 'bicycling')
 
 map + geom_segment(data = directions, aes(x = startLon, y = startLat, xend = endLon, yend = endLat))
 
@@ -91,9 +91,9 @@ setwd("zipcode_shapefile") #Folder with shape files.
 
 ## Load Shape File and Prepare for Plotting
 gpclibPermit() #Needed to get the fortify function to work
-shapefile<-readShapeSpatial("tl_2010_11_zcta510.shp") #Load Shape File
+shapefile <- readShapeSpatial("tl_2010_11_zcta510.shp") #Load Shape File
 shapefile@data$id = rownames(shapefile@data) #Prep Shape File
-dc.points<-fortify(shapefile, region = "id") #Prepare to work with ggplot
+dc.points <- fortify(shapefile, region = "id") #Prepare to work with ggplot
 
 ## Add Stations and zipcodes
 map + geom_path(aes(x = long, y = lat, group = group), data = dc.points, size = .3) + geom_point(data = bike, aes(x = longitude, y = latitude))
--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # ggmap-presentation  
 
-This repository contains all the files used for the presentation on ggmap given at the Statistical Programing DC Meet Up in March 2015.
+This repository contains all the files used for the presentation on ggmap given at the Statistical Programming DC Meet Up in March 2015.
+
+**Update January 2016**  
+Updates to **ggmap** have changed the default service for geocoding locations.  The default service is the Data Science Toolkit.  [Unfortunately Data Science Toolkit does not appear to be up and running any longer](https://github.com/dkahle/ggmap/issues/73).  The presentation code has been updated to specify Google as the source for geocodeing.  
 
 
 ## To Follow Along During the Presentation  
@@ -11,11 +14,11 @@ This repository contains all the files used for the presentation on ggmap given 
 2. Open the R Studio Project.  
 3. Install the *ggplot2*, *ggmap* and *data.table* packages.  
 4. Before the presentation while you have an internet connection open Presentation_Code.R and run lines 40, 61 and 67.  This will provide the background maps used during the presentation.  
-5. *Do Not Close your R Session.*  R will continue to hold the maps in memmory which you can use in the presentation.  
+5. *Do Not Close your R Session.*  R will continue to hold the maps in memory which you can use in the presentation.  
 
 ## Files Included  
-1. ggmap-presentation.Rproj: R Studio Project  
-2. Presentation_Code.R: A copy of the code run demonstrated during the talk.  
-3. slides.Rpres: A copy of the R Markdown code used to create the HTML slides used during the presentation.    
-4. presentation_data.csv: Data used druing the presentation.  Contains the daily average of trips from bike share stations in Washington, DC.  
-5. zipcode_shapefile: Contains Shape Files for Washington DC.  Used in the presentation slides.    
+1. **ggmap-presentation.Rproj:** R Studio Project  
+2. **Presentation_Code.R:** A copy of the code run demonstrated during the talk.  
+3. **slides.Rpres:** A copy of the R Markdown code used to create the HTML slides used during the presentation.    
+4. **presentation_data.csv:** Data used during the presentation.  Contains the daily average of trips from bike share stations in Washington, DC.  
+5. **zipcode_shapefile:** Contains Shape Files for Washington DC.  Used in the presentation slides.    
